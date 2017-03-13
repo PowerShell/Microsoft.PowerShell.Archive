5f68dfb39cfae5326e5391795a16e5cb2a5c45f6
Andrew <anmenaga@microsoft.com>
2017-03-13 16:05:51 -0700

    Travis CI - part 3

---
+++ TravisCI.ps1
@@ -0,0 +1,6 @@
+﻿$testResultsFile = ".\ArchiveTestResults.xml"
+Import-Module "C:\projects\Archive-Module\Microsoft.PowerShell.Archive" -Force
+$testResults = Invoke-Pester -Script "C:\projects\Archive-Module\Tests" -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+if ($testResults.FailedCount -gt 0) {
+    throw "$($testResults.FailedCount) tests failed."
+}
