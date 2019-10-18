902582d6aa3f9334cc08766ebc4c16e8b0adf131
Juan Romero <romero126@gmail.com>
2019-10-18 14:38:54 -0700

    Set Foundation for PR

---
+++ src/Microsoft.PowerShell.Archive/ArchiveProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+
+namespace Microsoft.PowerShell.Archive
+{
+    #region ArchiveProvider
+    public class ArchiveProvider :  NavigationCmdletProvider
+    {
+        protected override bool IsValidPath(string path)
+        {
+            return false;
+        }
+        
+    }
+    #endregion ArchiveProvider
+}
