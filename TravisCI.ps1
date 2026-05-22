9b59238bd11bce688861f10945996a7a645d2c0f
Anam Navied <anam.naviyou@gmail.com>
2026-05-22 11:47:38 -0400

    Add Github workflow for running CI tests (#159)

--- TravisCI.ps1
+++
@@ -1,6 +0,0 @@
-﻿$testResultsFile = "./ArchiveTestResults.xml"
-Import-Module "./Microsoft.PowerShell.Archive/Microsoft.PowerShell.Archive.psd1" -Force
-$testResults = Invoke-Pester -Script "./Tests" -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
-if ($testResults.FailedCount -gt 0) {
-    throw "$($testResults.FailedCount) tests failed."
-}
